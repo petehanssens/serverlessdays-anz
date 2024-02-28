17624a1408497e091ae6e7a8bce9576ad659f3d5
Stephen Sennett <ssennett@ssennett.net>
2024-02-29 00:39:26 +1100

    Refactoring organisers data into mono-JSON

---
+++ src/organisers/organisers.11tydata.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "layout": "organiser.njk",
+    "page": {
+        "title": "ServerlessDays ANZ - Organiser",
+        "description": "One Day. One Track. One Community.",
+        "url": "https://anz.serverlessdays.io/organisers/"
+    }
+};
