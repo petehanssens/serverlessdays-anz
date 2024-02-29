6565a17137318fabb2d35c11d8ea9bb412044a1f
Stephen Sennett <ssennett@ssennett.net>
2024-02-29 13:39:53 +1100

    Major Updates - End of Feb (#73)

---
+++ src/speakers/speakers.11tydata.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "layout": "speaker.njk",
+    "page": {
+        "title": "ServerlessDays ANZ - Speakers",
+        "description": "One Day. One Track. One Community.",
+        "url": "https://anz.serverlessdays.io/speakers"
+    }
+};
